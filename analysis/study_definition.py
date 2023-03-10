835c99b224529ed0546486b3b499272829f226b3
drlspencer <lucy.spencer@dhsc.gov.uk>
2023-03-10 14:07:06 +0000

    Solved codelist issue

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -1,5 +1,10 @@
 from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv  # NOQA
+# from codelists import *
 
+teratogenic_drug_codes = codelist_from_csv(
+    "codelists/opensafely-teratogenic-medicines.csv",
+    system="snomed",
+    column="dmd_id")
 
 study = StudyDefinition(
     default_expectations={
@@ -7,15 +12,41 @@ study = StudyDefinition(
         "rate": "uniform",
         "incidence": 0.5,
     },
-    population=patients.registered_with_one_practice_between(
-        "2019-02-01", "2020-02-01"
-    ),
+
+    # population=patients.registered_with_one_practice_between(
+    #     "2019-02-01", "2020-02-01"
+    # ),
+
+    population=patients.all(),
 
     age=patients.age_as_of(
-        "2019-09-01",
+        "2022-09-01",
         return_expectations={
             "rate": "universal",
             "int": {"distribution": "population_ages"},
         },
     ),
+
+    example=patients.with_these_medications(
+        teratogenic_drug_codes,
+        return_expectations={
+           "int": {"distribution": "normal", "mean": 2, "stddev": 1},
+           "incidence": 0.2,
+        },
+        on_or_before=None,
+        on_or_after=None,
+        between=None,
+        find_first_match_in_period=None,
+        find_last_match_in_period=None,
+        returning='binary_flag',
+        include_date_of_match=True,
+        date_format=None,
+        ignore_days_where_these_clinical_codes_occur=None,
+        episode_defined_as=None,
+        return_binary_flag=None,
+        return_number_of_matches_in_period=False,
+        return_first_date_in_period=False,
+        return_last_date_in_period=False,
+        include_month=False,
+        include_day=False)
 )
--- analysis/study_definition_practice.py
+++ analysis/study_definition_practice.py
@@ -1,5 +1,9 @@
 from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv  # NOQA
 
+teratogenic_drug_codes = codelist_from_csv(
+    "codelists/opensafely-teratogenic-medicines.csv", system="ctv3", column="CTV3ID"
+)
+
 study = StudyDefinition(
     # define default dummy data behaviour
     default_expectations={
@@ -15,7 +19,7 @@ study = StudyDefinition(
     population=patients.all(),
 
     valproate_count=patients.with_these_medications(
-    oral_steroid_med_codes,
+    teratogenic_drug_codes,
     between=["2019-03-01", "2020-02-29"],
     ignore_days_where_these_clinical_codes_occur=copd_reviews,
     returning="number_of_episodes",
@@ -24,6 +28,31 @@ study = StudyDefinition(
         "int": {"distribution": "normal", "mean": 2, "stddev": 1},
         "incidence": 0.2,
     },
+
+
+    example=with_these_medications(
+        teratogenic_drug_codes, 
+        return_expectations={
+            "int": {"distribution": "normal", "mean": 2, "stddev": 1},
+            "incidence": 0.2,
+        },
+        on_or_before=None, 
+        on_or_after=None, 
+        between=None, 
+        find_first_match_in_period=None, 
+        find_last_match_in_period=None, 
+        returning='binary_flag', 
+        include_date_of_match=False, 
+        date_format=None, 
+        ignore_days_where_these_clinical_codes_occur=None, 
+        episode_defined_as=None, 
+        return_binary_flag=None, 
+        return_number_of_matches_in_period=False, 
+        return_first_date_in_period=False, 
+        return_last_date_in_period=False, 
+        include_month=False, 
+        include_day=False)
+
 )
 
     # define the study variables
